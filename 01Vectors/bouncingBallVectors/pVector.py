3a160bd965532428228cecdcd0649b6cb40a845f
Alberto Barradas <abcsds@gmail.com>
2015-12-16 00:32:22 -0600

    Remove unused pVector file

--- 01Vectors/bouncingBallVectors/pVector.py
+++
@@ -1,8 +0,0 @@
-class PVector(x, y):
-    """2D vector"""
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    def add(PVector v):
-        self.x += v.x
-        self.y += v.y
